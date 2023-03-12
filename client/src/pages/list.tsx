f7a4ac7da55d3d38f0658468f96ee40dab2329a4
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-03-12 13:11:14 +0530

    refactor: use ant design list

--- client/src/pages/list.tsx
+++ client/src/pages/list.tsx
@@ -1,5 +1,12 @@
+import { RefreshButton } from '@pankod/refine-mui';
 import React, { useEffect, useState } from 'react';
 import clubs from "../crd.json"
+
+import {
+    List,
+    Table,
+} from "@pankod/refine-antd";
+
 interface IData {
     success: boolean;
     error: boolean;
@@ -32,27 +39,62 @@ interface IData {
     };
 }
 
+interface IResults {
+    event_participated: {
+        event_id: string;
+        event_title: string;
+    };
+    team_leader: {
+        id: string;
+        aura_id: string;
+        usn: string;
+        name: string;
+        email: string;
+    };
+    _id: string;
+    team_name: string;
+    team_members: Array<{
+        usn: string;
+        name: string;
+    }>;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+};
+
 interface IUser {
     name: string;
     email: string;
     avatar: string;
 }
 
+let paginationTs: any = Date.now();
+let hasMoreResults = true;
+
 export const PostList = () => {
-    const [data, setData] = useState<IData | null>(null);
+    const [data, setData] = useState<Array<IResults>>([]);
     const [user, setUser] = useState<IUser | null>(null);
 
+    async function loadTeams() {
+        if (!hasMoreResults)
+            return;
+
+        try {
+            const response = await fetch(`http://localhost:4000/teams${paginationTs ? `?paginationTs=${paginationTs}` : ""}`);
+            const json = await response.json();
+
+            // Update paginationTs
+            paginationTs = json.data.paginationTs;
+            hasMoreResults = paginationTs !== null;
+
+            setData([...data, ...json.data.results]);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:4000/Teams');
-                const json = await response.json();
-                setData(json);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-        fetchData();
+        loadTeams();
     }, []);
 
     useEffect(() => {
@@ -79,38 +121,47 @@ export const PostList = () => {
                 </div>
             )}
 
-            <h1>Data:</h1>
+            <List>
+                <Table dataSource={data} pagination={false}>
+                    <Table.Column
+                        dataIndex="_id"
+                        title="Team #"
+                        render={(id, _, index) => <span>{index + 1}</span>}
+                    />
+                    <Table.Column
+                        dataIndex="team_name"
+                        title="Team Name"
+                        render={(name) => <span>{name}</span>}
+                    />
+                    <Table.Column
+                        dataIndex="team_leader"
+                        title="Team Leader Name"
+                        render={(team_leader) => <span>{team_leader.name}</span>}
+                    />
+                    <Table.Column
+                        dataIndex="team_leader"
+                        title="Team Leader Aura ID"
+                        render={(team_leader) => <span>{team_leader.aura_id}</span>}
+                    />
+                    <Table.Column
+                        dataIndex="team_members"
+                        title="Team Members"
+                        render={(members: Array<any>) =>
+                            members.length === 0 ? <span>None</span> :
+                                <ul>
+                                    {members.map(member => <li>{member.name}</li>)}
+                                </ul>
+                        }
+                    />
+                </Table>
+            </List>
 
-            <table>
-                <thead>
-                <tr>
-                    <th>Team No</th>
-                    <th>Team Name</th>
-                    <th>Team Leader Name</th>
-                    <th>Team Leader USN</th>
-                    <th>Team Members</th>
-                </tr>
-                </thead>
-                <tbody>
-                {data.data.results.map((result, index) => (
-                    <tr key={result._id}>
-                        <td>{index + 1}</td>
-                        <td>{result.team_name}</td>
-                        <td>{result.team_leader.name}</td>
-                        <td>{result.team_leader.usn}</td>
-                        <td>
-                            <ul>
-                                {result.team_members.map((member) => (
-                                    <li key={member.usn}>
-                                        {member.name} ({member.usn})
-                                    </li>
-                                ))}
-                            </ul>
-                        </td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
+            <br />
+            <RefreshButton fullWidth={true} onClick={() => {
+                loadTeams();
+            }} disabled={!hasMoreResults}>
+                {hasMoreResults ? "Load next 20 results" : "No more results!"}
+            </RefreshButton>
         </div>
     );
 };
