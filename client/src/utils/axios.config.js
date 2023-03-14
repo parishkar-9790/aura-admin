2af49141ebf0d1ff7c8b4dda863eccf5e8e11fd6
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-03-14 23:42:12 +0530

    added create team ui

---
+++ client/src/utils/axios.config.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3001",
+  // baseURL: "http://3.110.70.229:3001",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  withCredentials: true,
+});
+
+export default api;
